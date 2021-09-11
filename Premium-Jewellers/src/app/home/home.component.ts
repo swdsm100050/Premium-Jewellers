2f5b46b7a27d9f563b425fb28fdc2b6e95eaadaf
swdsm100050 <swdsm1000@gmail.com>
2021-09-11 19:00:48 +0530

    home page

--- Premium-Jewellers/src/app/home/home.component.ts
+++ Premium-Jewellers/src/app/home/home.component.ts
@@ -12,4 +12,24 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selected=[
+    {"imgUrl":"../../assets/home/p1.jpg","title":"CLASSIC DIAMOND RING"},
+    {"imgUrl":"../../assets/home/p2.jpg","title":"CLASSIC DIAMOND RING"},
+    {"imgUrl":"../../assets/home/p3.jpg","title":"CLASSIC DIAMOND RING"},
+    {"imgUrl":"../../assets/home/p4.jpg","title":"CLASSIC DIAMOND RING"},
+  ]
+  collections=[
+    {"imgUrl":"../../assets/home/collections1.jpg","title":"COLLECTION-1"},
+    {"imgUrl":"../../assets/home/collections2.jpg","title":"COLLECTION-2"},
+    {"imgUrl":"../../assets/home/collections3.jpg","title":"COLLECTION-3"},
+    {"imgUrl":"../../assets/home/collections4.jpg","title":"COLLECTION-4"},
+  ]
+  why=[
+    {"imgUrl":"../../assets/home/icon1.png","title":"EXCELLENT QUALITY"},
+    {"imgUrl":"../../assets/home/icon2.png","title":"100% Certified Jewellery"},
+    {"imgUrl":"../../assets/home/icon3.png","title":"30 Money Back Guarantee"},
+    {"imgUrl":"../../assets/home/icon4.png","title":"Life Time Exchange & BuyBack"},
+    {"imgUrl":"../../assets/home/icon5.png","title":"Free One Year Insurance"},
+    {"imgUrl":"../../assets/home/icon6.png","title":"Free Shipping"},
+  ]
 }
--- Premium-Jewellers/src/app/shared/components/header/header.component.ts
+++ Premium-Jewellers/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  isSticky = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  @HostListener('document:scroll')
+  scrollFunction() {
+    document.body.scrollTop > 60 || document.documentElement.scrollTop > 60
+      ? (this.isSticky = true)
+      : (this.isSticky = false);
+  }
 }
